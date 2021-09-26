205b922b812bfa08c5808f4f76b71f30145ef32e
qin.batista <qin.batista@outlook.com>
2021-09-25 17:57:54 -0700

    base

---
+++ DataStructure/IntroOfC/main.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+struct pokeman
+{
+    char* name;
+    int dex_num;
+};
+
+void printwhatipass(int x);
+int main()
+{
+    int a = 5;
+    char* b = "aaaa";
+    float pi = 3.1444444;
+    printf("Int a = %d\n", a);
+    printf("String b = %s\n", b);
+    printf("Float f = %f\n", pi);
+    printf("1: %d\n2: %s\n3: %4.3f\n", a, b, pi);
+    printf("hello world\n");
+
+    if(5!=6)
+    {
+        printf("True!\n");
+    }
+    else if(5!=6)
+    {
+
+    }
+    else
+    {
+
+    }
+    for(int i = 0; i<5; i++)
+    {
+        printf("i = %d\n", i);
+    }
+    // while (1)
+    // {
+    //     /* code */
+    // }
+    printwhatipass(20);
+    struct pokeman pika = {.name = "Sparky", .dex_num = 25};
+
+    pika.name = "Bolt";
+    printf("Pokemon's name: %s\n", pika.name);
+    int t = 20;
+    int* t_ptr = &t;
+
+    printf("Pointer address: %p\n", t_ptr);
+    printf("Pointer value: %d\n", *t_ptr);
+    return 0;
+}
+
+void printwhatipass(int x)
+{
+    printf("passed %d\n", x);
+}
